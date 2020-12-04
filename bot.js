e9df8ab0bae9c16e79c8305a28faff3d18040a0d
fowwo <54595444+fowwo@users.noreply.github.com>
2020-12-04 17:54:56 -0600

    Add command support

--- bot.js
+++ bot.js
@@ -2,9 +2,23 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+var config = {
+	prefix: "!"
+}
+
 const token = fs.readFileSync("token.txt", "utf-8").trim();
 client.login(token);
 
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 
-client.on('message', (msg) => {});
+client.on('message', (msg) => {
+	if (msg.content.startsWith(config.prefix)) {
+		let args = msg.content.split(" ");
+		let command = args.shift().substring(config.prefix.length);
+		switch (command) {
+			default:
+				// Invalid command
+				break;
+		}
+	}
+});
