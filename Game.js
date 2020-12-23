973cc5091a347461c9267d81a625f029a8c0c4e9
fowwo <54595444+fowwo@users.noreply.github.com>
2020-12-22 19:32:06 -0600

    Add Duel challenge method

--- Game.js
+++ Game.js
@@ -1,5 +1,10 @@
 const Discord = require("discord.js");
 
+const defaultColor = "#faa61a";
+const failColor = "#ff0000";
+const cancelledColor = "#606060";
+const badMessageTimeout = 5000;
+
 /**
  * Models an abstract game.
  */
@@ -48,6 +53,94 @@ class Duel extends Game {
 		this.challenger = challenger;
 		this.opponent = opponent;
 		this.allowBotOpponent = allowBotOpponent;
+
+		// Required methods
+		if (this.play === undefined) throw new Error(`${new.target.name} is missing the 'play(message)' method.`);
+	}
+
+	/**
+	 * Creates a challenge request.
+	 * @param {Number} timeout - How long the challenge request will last in milliseconds.
+	 */
+	challenge(timeout = 90000) {
+		if (this.challenger.id === this.opponent.id) {
+			this.channel.send(new Discord.MessageEmbed({ 
+				title: this.toString(),
+				description: `:no_entry_sign: You can't challenge yourself, ${this.challenger.toString()}!`,
+				color: failColor
+			})).then((message) => { setTimeout(() => { message.delete(); }, badMessageTimeout); });
+		} else if (this.challenger.bot) {
+			this.channel.send(new Discord.MessageEmbed({ 
+				title: this.toString(),
+				description: `:no_entry_sign: Bots can't initiate challenges!`,
+				color: failColor
+			})).then((message) => { setTimeout(() => { message.delete(); }, badMessageTimeout); });
+		} else if (this.opponent.bot) {
+			if (!this.allowBotOpponent) {
+				this.channel.send(new Discord.MessageEmbed({ 
+					title: this.toString(),
+					description: `:no_entry_sign: You can't challenge bots, ${this.challenger.toString()}.`,
+					color: failColor
+				})).then((message) => { setTimeout(() => { message.delete(); }, badMessageTimeout); });
+			} else if (this.opponent.id === this.client.user.id) {
+				this.play();
+			} else {
+				this.channel.send(new Discord.MessageEmbed({ 
+					title: this.toString(),
+					description: `:no_entry_sign: You can't challenge other bots, ${this.challenger.toString()}.`,
+					color: failColor
+				})).then((message) => { setTimeout(() => { message.delete(); }, badMessageTimeout); });
+			}
+		} else {
+			this.channel.send(new Discord.MessageEmbed({ 
+				title: this.toString(),
+				description: `:crossed_swords: ${this.challenger.toString()} is challenging ${this.opponent.toString()} to a game of ${this.title}!`,
+				footer: { text: `${this.opponent.username} has ${timeout / 1000} second${timeout / 1000 === 1 ? "" : "s"} to accept.` },
+				color: defaultColor
+			})).then((message) => {
+				message.react("✅");
+				message.react("❌");
+				let timer = setTimeout(() => {
+					message.reactions.removeAll();
+					message.edit(new Discord.MessageEmbed({ 
+						title: this.toString(),
+						description: `:clock5: ${this.challenger.toString()}'s challenge to ${this.opponent.toString()} has timed out.`,
+						footer: { text: `${this.opponent.username} did not accept in time.` },
+						color: cancelledColor
+					}));
+					collector.stop();
+				}, timeout);
+				let collector = message.createReactionCollector((reaction, user) => 
+					(user.id === this.challenger.id && reaction.emoji.name === "❌") ||
+					(user.id === this.opponent.id && (reaction.emoji.name === "✅" || reaction.emoji.name === "❌"))
+				).once("collect", (reaction, user) => {
+					clearTimeout(timer);
+					if (reaction.emoji.name === "✅") {
+	
+						// Challenge accepted
+						message.reactions.removeAll();
+						this.play(message);
+	
+					} else if (user.id === this.opponent.id) {
+	
+						// Challenge declined
+						message.reactions.removeAll();
+						message.edit(new Discord.MessageEmbed({ 
+							title: this.toString(),
+							description: `:no_entry_sign: ${this.opponent.toString()} declined ${this.challenger.toString()}'s challenge.`,
+							color: failColor
+						}));
+	
+					} else {
+	
+						// Challenge cancelled
+						message.delete();
+	
+					}
+					collector.stop();
+				});
+			});
+		}
 	}
 
 }
